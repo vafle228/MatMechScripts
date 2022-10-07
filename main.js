ef207967f7e727744fddcba1c9cf7e4f08efadb4
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-10-07 10:15:25 +0500

    finish

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-function numRange(start, end) {
-    return new Array(end - start).fill().map((val, index) => index + start);
-}
+function decodeMsg(msg) {
+    const bits_poses = associateBits(msg);
 
-function associateBits(code) {
-    const associated_poses = new Array();
-
-    for (let i = 0; Math.pow(2, i) < code.length; i++) {
-        associated_poses.push(new Array());
-        for (
-            let j = Math.pow(2, i) - 1;
-            j < code.length;
-            j += Math.pow(2, i + 1)
-        )
-            associated_poses[i].push(...numRange(j, j + Math.pow(2, i)));
+    const errors = new Array();
+    for (let i = 0; i < bits_poses.length; i++) {
+        const control_sum = bits_poses[i].reduce(
+            (prev, cur) => prev + +msg[cur], 0
+        );
+        control_sum % 2 !== 0 ? errors.push(bits_poses[i]) : undefined;
     }
 
-    return associated_poses;
+    alert(errors.length === 0 ? "NO ERRORS" : "ERROR");
+
+    if (errors.length !== 0) {
+        const error_pos =
+            errors.length === 1 ? errors[0][0] : getSameElement(...errors);
+        msg = replaceAt(msg, error_pos, (+msg[error_pos] + 1) % 2);
+    }
+
+    return msg;
 }
 
-function replaceAt(str, index, new_el) {
-    return str.substring(0, index) + new_el + str.substring(index);
-}
+function encodeMsg(msg) {
+    for (let i = 0; Math.pow(2, i) < msg.length; i++)
+        msg = addAt(msg, Math.pow(2, i) - 1, "0");
+    const bits_poses = associateBits(msg);
 
-const msg = "0010101";
-const bits_poses = associateBits(msg);
-
-const errors = new Array();
-for (let i = 0; i < bits_poses.length; i++) {
-    const control_sum = bits_poses[i].reduce(
-        (prev, cur) => prev + +msg[cur],
-        0
-    );
-    control_sum % 2 !== 0 ? errors.push(bits_poses[i]) : undefined;
-}
-
-if (errors.length === 0) console.log("No Errors");
-else if (errors.length === 1) {
-    console.log("ERROR");
-    console.log(replaceAt(msg, errors[0][0], (msg[errors[0][0]] + 1) % 2));
-} else {
-    console.log("ERROR");
+    for (let i = 0; i < bits_poses.length; i++)
+        msg = replaceAt(
+            msg,
+            bits_poses[i][0],
+            bits_poses[i].reduce((prev, cur) => prev + +msg[cur], 0) % 2
+        );
+    
+    return msg;
 }
