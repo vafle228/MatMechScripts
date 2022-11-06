853d8e4dc3d1e49cce1c791a1a3d5ac3a8060cda
~ Vafle ~ <60180871+NeiroFucker@users.noreply.github.com>
2022-11-06 17:26:56 +0500

    Update rubykarphash.js

--- HashFunctions/rubykarphash.js
+++ HashFunctions/rubykarphash.js
@@ -1,20 +1,20 @@
 const IHash = require("./basehash");
 
 class RubyKarpHash extends IHash {
-    static _base = 2;
-    static pose_k = (pose) => Math.pow(this._base, pose);
+    static _base = 7;
+    static pose_k = (pose) => Math.pow(this._base, pose - 1);
 
     static hash(string) {
         let hash = 0;
         for (let i = 0; i < string.length; i++)
-            hash += string.charCodeAt(i) * this.pose_k(string.length - i - 1);
+            hash += string.charCodeAt(i) * this.pose_k(string.length - i);
         return hash;
     }
 
     static recurent_hash(last_hash, last_str, new_str) {
         const hash_delta =
-            this.pose_k(last_str.length - 1) * last_str.charCodeAt(0);
-        return 2 * (last_hash - hash_delta) + new_str.charCodeAt(new_str.length - 1);
+            this.pose_k(last_str.length) * last_str.charCodeAt(0);
+        return this._base * (last_hash - hash_delta) + new_str.charCodeAt(new_str.length - 1);
     }
 }
 
