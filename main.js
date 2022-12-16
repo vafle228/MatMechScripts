760b229d4f748bf014bc20cbaeef5914bd986e36
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-12-16 11:34:01 +0500

    Validate programfile

--- main.js
+++ main.js
@@ -2,16 +2,27 @@ const fs = require("fs");
 const ProgrammMemory = require("./SirMemory/main");
 const ProgrammExecutor = require("./SirComands/main");
 
-const memory = new ProgrammMemory(fs.readFileSync(process.argv[2], "utf8"));
-const executor = new ProgrammExecutor(memory);
-
-let i = 0;
-while (memory.read("$" + i) != "endscript" && i != memory.length()) {
-    const command = memory.read("$" + i);
-    const opperands_count = executor[command].length;
-
-    const result = executor[command](
-        ...memory.readChunk("$" + (i + 1), "$" + (opperands_count + i + 1))
-    );
-    i = result ? result : i + 1 + opperands_count;  // goto handler
+function validateData(programfile) {
+    return fs.existsSync(programfile);
 }
+
+const programfile = process.argv[2];
+if (validateData(programfile)) {
+    const program = fs.readFileSync(programfile, "utf-8")
+    
+    const memory = new ProgrammMemory(program);
+    const executor = new ProgrammExecutor(memory);
+
+    let i = 0;
+    while (memory.read("$" + i) != "endscript" && i != memory.length()) {
+        const command = memory.read("$" + i);
+        const opperands_count = executor[command].length;
+
+        const result = executor[command](
+            ...memory.readChunk("$" + (i + 1), "$" + (opperands_count + i + 1))
+        );
+        i = result ? result : i + 1 + opperands_count;  // goto handler
+    }
+}
+
+else { console.log("Something went wrong! Check your args"); }
