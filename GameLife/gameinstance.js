2e34b4504617c3d5a8fe898447a2bf4c27512179
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-12-19 11:47:15 +0500

    init struct

---
+++ GameLife/gameinstance.js
@@ -0,0 +1,8 @@
+export default class GameInstance {
+    constructor(width, height) {
+        this._field = new Array(width);
+
+        for (let i = 0; i < width; i++)
+            this._field[i] = new Array(height);
+    }
+}
